45e88f40e6b0a0bce64be133db216cc1a7f0d49f
Rosembergth Ordoñez Mujica <86086718+rosembergth@users.noreply.github.com>
2021-10-04 03:21:13 -0500

    Incluye los LED encendidos

--- main.ts
+++ main.ts
@@ -8,6 +8,14 @@ basic.forever(function () {
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 1)
         pins.digitalWritePin(DigitalPin.P2, 1)
+        led.setBrightness(255)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
         basic.pause(10000)
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
